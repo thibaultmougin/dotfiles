4458b3e1f6e06b57a3ce8f07991d0fbfa3e60257
Thibault Mougin <thibault.mougin@ensta-paris.fr>
2025-03-09 01:00:57 +0100

    Maj des vsix, ajout script install extensions et ù au lieu de t dans les keybinds

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-code --install-extension vscode/extensions/*
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo " "
+      echo "$package dotfiles install"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-e, --extensions          install vscode extensions"
+      exit 0
+      ;;
+    --extensions|-e)
+
+        find vscode/extensions/ -type f -print0 | while IFS= read -r -d $'\0' file;
+          do code --install-extension "$file" ;
+        done
+        exit 1
+      ;;
+  esac
+done
